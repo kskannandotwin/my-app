c1430af4ecd7089562863fa5e0b1757c76e15f92
kskannandotwin <kannan@jobuli.com>
2025-09-11 21:10:22 +0530

    11.09.2025
    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import HelloWorld from "./core-concepts/01-hello-world/HelloWorld";
 import GreetingClass from "./core-concepts/02-components/GreetingClass";
-import JsxExample from "./core-concepts/jsx/JsxExample";
+import PropsExample from "./core-concepts/04-props/PropsExample";
+import JsxExample from "./core-concepts/03-jsx/JsxExample";
 
 function App() {
   // return <HelloWorld />;
   // return <Greeting />;
   // return <GreetingClass />;
-  return <JsxExample />;
+  // return <JsxExample />;
+  return (
+    <PropsExample technology="Javascript" title="Welcome to React Course" />
+  );
 }
 
 export default App;
---
+++ src/core-concepts/04-props/PropsExample.jsx
@@ -0,0 +1,9 @@
+export default function PropsExample({ title, technology }) {
+  return (
+    <>
+      <h1>Props Example</h1>
+      <p>{title}</p>
+      <p>Learning {technology} is fun!</p>
+    </>
+  );
+}
